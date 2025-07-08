8aea013141cd618814a2e818a8ff6d56b2707cc4
RyXWare <justinplatasch@gmail.com>
2025-07-09 02:59:44 +0800

    Update Bullet-tp.lua

--- Bullet-tp.lua
+++ Bullet-tp.lua
@@ -1,5 +1,7 @@
+local StarterGui = game:GetService("StarterGui")
+
 StarterGui:SetCore("SendNotification", {
     Title = "im working on it",
-    Text = "well probably not because im in school",
+    Text = "well probably not because im probably in school rn",
     Duration = 20
 })
